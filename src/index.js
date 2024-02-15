46049ad828ebf45629f150f350454859fd8ab9ee
Siddartha Koppaka <siddarthakoppaka@gmail.com>
2024-02-15 16:14:21 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+const { add } = require('./arithmetica');
 
-// Serve the static HTML file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use(cors());
+app.get('/', (req, res) => { 
+    res.send('Arithmetic service - Hello world!');
+ });
+
+app.get('/add/:a/:b', (req, res) => { 
+    //res.json(Number(req.params.a) + Number(req.params.b)); 
+    let n = Number(req.params.a);
+    let m = Number(req.params.b);
+    let sum = add(n, m);
+    res.json(sum);
 });
 
-// Endpoint to add numbers
-app.get('/add', (req, res) => {
-  const num1 = parseFloat(req.query.num1);
-  const num2 = parseFloat(req.query.num2);
-  if (isNaN(num1) || isNaN(num2)) {
-    return res.status(400).send('Both inputs must be valid numbers.');
-  }
-  const sum = num1 + num2;
-  res.send({ sum: sum }); // Send the sum as a JSON response
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port);
